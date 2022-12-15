3ced87cd93f5ce6c719054ef38acf8a045d9b21f
Adityasetiawann <kodityasetiawan@gmail.com>
2022-12-15 22:25:30 +0700

    mpa

--- angular_172101880/src/app/app-routing.module.ts
+++ angular_172101880/src/app/app-routing.module.ts
@@ -3,17 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { Dashboard2Component } from './dashboard2/dashboard2.component';
+import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './login/login.component';
 import { MahasiwaComponent } from './mahasiwa/mahasiwa.component';
 
 const routes: Routes = [
-  { path: "", redirectTo: "login", pathMatch: "full" },
   { path: "admin", component: AdminComponent  },
   { path: "dashboard", component: DashboardComponent },
   { path: "dashboard2", component: Dashboard2Component },
+  { path: "mahasiswa", component: MahasiwaComponent },
   { path: "login", component: LoginComponent },
-  { path: "mahasiswa", component: MahasiwaComponent }
-  
+  { path: "", redirectTo: "login", pathMatch: "full" }
 ];
 
 @NgModule({
