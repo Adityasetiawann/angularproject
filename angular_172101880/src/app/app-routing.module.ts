df3ae1d82cff5ed1ddeb54c62385f2f0569e0455
Adityasetiawann <kodityasetiawan@gmail.com>
2022-12-16 10:14:40 +0700

    otentikasi

--- angular_172101880/src/app/app-routing.module.ts
+++ angular_172101880/src/app/app-routing.module.ts
@@ -6,12 +6,13 @@ import { Dashboard2Component } from './dashboard2/dashboard2.component';
 import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './login/login.component';
 import { MahasiwaComponent } from './mahasiwa/mahasiwa.component';
+import { OtentikasiGuard } from './otentikasi.guard';
 
 const routes: Routes = [
   { path: "admin", component: AdminComponent  },
-  { path: "dashboard", component: DashboardComponent },
-  { path: "dashboard2", component: Dashboard2Component },
-  { path: "mahasiswa", component: MahasiwaComponent },
+  { path: "dashboard", component: DashboardComponent, canActivate : [OtentikasiGuard]}, 
+  { path: "dashboard2", component: Dashboard2Component, canActivate : [OtentikasiGuard] },
+  { path: "mahasiswa", component: MahasiwaComponent, canActivate : [OtentikasiGuard] },
   { path: "login", component: LoginComponent },
   { path: "", redirectTo: "login", pathMatch: "full" }
 ];
